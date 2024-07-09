796b61540e1c439a3da27c476d96a98bbd732d89
georgecretu26 <narciscretu26@gmail.com>
2024-07-09 07:58:40 +0200

    feat(frontend, hooks): create hooks

---
+++ apps/frontend/src/hooks/index.ts
@@ -0,0 +1,7 @@
+export { default as useAuth } from "./use-auth";
+export { default as useFetchShop } from "./use-fetch-shop";
+export { default as useFetchAllShops } from "./use-fetch-all-shops";
+export { default as useDeleteShop } from "./use-delete-shop";
+export { default as useCreateShop } from "./use-create-shop";
+export { default as useUpdateShop } from "./use-update-shop";
+export { default as useResetDatabase } from "./use-reset-db";
